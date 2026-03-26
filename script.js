e2b643361b29f9d5c2627f4d0233c95c7f8d25b3
Lakshya Mittal <lakshyamittal171@gmail.com>
2026-03-27 05:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const number = document.querySelector('#time')
+const btn = document.querySelector('.btn')
+const ISO = document.querySelector('.ISO')
+const UTC = document.querySelector('.UTC')
+const yearsAgo = document.querySelector('.yearsAgo')
+const secondsAgo = document.querySelector('.secondsAgo')
+const Local = document.querySelector('.Local')
+
+
+btn.addEventListener('click', () => {
+    if (number.value.trim()) {
+        const value = Number(number.value.trim())
+        const date = new Date(value)
+        
+        ISO.innerText = date.toISOString()
+        UTC.innerText = date.toUTCString()
+        Local.innerText = date
+        yearsAgo.innerText = date.getFullYear() - 1970 + ' yearsAgo'
+        secondsAgo.innerText = Math.floor((Date.now() - value)/1000) + ' secondsAgo'
+
+    }
+})
